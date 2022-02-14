f60a26debd68407a628e88a05554e9411cabfa79
Imroz <imroz.khan@finra.org>
2022-02-14 14:21:29 -0500

    Updating heading color and adding quick links

--- src/app/components/regulators/regulators.component.ts
+++ src/app/components/regulators/regulators.component.ts
@@ -45,7 +45,7 @@ export class RegulatorsComponent implements OnInit, OnDestroy {
   setRegulatorKeys() {
     this.allKeys = (Object.keys(this.regulatorData[0])).filter(
       keyItem => keyItem !== 'Entity' && keyItem !== 'Short Name' && keyItem !== 'Entity Url'
-      && keyItem !== 'Foreign Name'
+      && keyItem !== 'Foreign Name' && keyItem !== 'Twitter'
     );
   }
 
